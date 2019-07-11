9fcf1bfa30277ad979abef1e93a37125437f8e0b
pixelgoo <pixelgoo@gmail.com>
2019-07-11 17:33:56 +0300

    Do not digest images

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module D2league
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.digest = false  
   end
 end
