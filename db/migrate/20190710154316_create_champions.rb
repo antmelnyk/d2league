aed475e4302463fd2b09308f6a895fe0a72dbaf8
pixelgoo <pixelgoo@gmail.com>
2019-07-11 14:51:37 +0300

    Update Champion table

--- db/migrate/20190710154316_create_champions.rb
+++ db/migrate/20190710154316_create_champions.rb
@@ -3,10 +3,15 @@ class CreateChampions < ActiveRecord::Migration[6.0]
     create_table :champions do |t|
       t.string :name
       t.string :passive
+      t.string :passive_description
       t.string :q
+      t.string :q_description
       t.string :w
+      t.string :w_description
       t.string :e
+      t.string :e_description
       t.string :r
+      t.string :r_description
 
       t.timestamps
     end
