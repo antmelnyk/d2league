b7b86acc79524d269607a42a2631ed22e0af7042
pixelgoo <pixelgoo@gmail.com>
2019-07-18 01:20:02 +0300

    Do not use asset pipeline for images

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module D2league
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-    config.assets.digest = false  
+    # the framework and any gems in your application. 
   end
 end
