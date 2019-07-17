a1152c45cf7269f8f282408da353ea5df6549bc5
pixelgoo <pixelgoo@gmail.com>
2019-07-17 03:44:47 +0300

    Sort heroes

--- app/controllers/API/heroes_controller.rb
+++ app/controllers/API/heroes_controller.rb
@@ -1,7 +1,7 @@
 class API::HeroesController < ApplicationController
 
   def index
-    heroes = Hero.all.to_json(only: [:id, :name])
+    heroes = Hero.all.sort_by(&:name).to_json(only: [:id, :name])
     render json: heroes
   end
   
