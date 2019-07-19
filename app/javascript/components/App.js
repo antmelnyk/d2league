ae26d02829cd3346f04a00dcd2b2e2e7eae19cff
pixelgoo <pixelgoo@gmail.com>
2019-07-19 23:54:51 +0300

    Add footer

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -13,12 +13,37 @@ class App extends React.Component {
     return (
       <Provider store={store}>
         <main className="app-wrapper">
+
           <BrowserRouter>
             <Switch>
               <Route exact path="/" render={() => <Home />} />
               <Route path="/suggestion" render={() => <Suggestion />} />
             </Switch>
           </BrowserRouter>
+
+          <footer className="footer">
+            <div className="container">
+              <div className="footer__disclaimer">
+                This is a fan site. It has no official relation with Valve or Riot Games and isn't promoting anything.
+              </div>
+            
+              <a
+                className="footer__source"
+                target="_blank"
+                href="https://github.com/pixelgoo/d2league">
+                Source code
+                <img src="assets/github-logo.png" />
+              </a>
+              <a 
+                className="footer__donate"
+                target="_blank"
+                href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=pixelgoo%40gmail.com&item_name=d2league.fun+appreciation%21&currency_code=USD&source=url">
+                  Buy me a beer 🍺
+              </a>
+              <a className="footer__contact" href="mailto:pixelgoo@gmail.com">Contact me 📧</a>
+            </div>
+          </footer>
+
         </main>
       </Provider>
     );
